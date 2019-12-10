b55ed96f3a3ea1caa54e62b4d569a390b9d098af
Jorliman <jorliman@gmail.com>
2019-12-10 15:12:21 +0800

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Tommy Li on 10/12/2019.
+//  Copyright © 2019 Tommy Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
